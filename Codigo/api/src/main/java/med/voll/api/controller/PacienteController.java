f83a871a658813a9b5ad614ac0b05e4c0041c555
klayton-a-souza <klayton.al.souza@gmail.com>
2023-01-24 11:09:16 -0300

    Codigo 03: Spring Data JPA

---
+++ Codigo/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,24 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.DadosCadastroPacientes;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid DadosCadastroPacientes dados){
+        repository.save(new Paciente(dados));
+    }
+}

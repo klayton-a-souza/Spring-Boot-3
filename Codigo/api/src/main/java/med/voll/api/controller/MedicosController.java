b2d67124393c02f964054c74cec928820ad2f439
klayton-a-souza <klayton.al.souza@gmail.com>
2023-01-24 14:13:48 -0300

    Codigo 1.1: Produção de dados na API

--- Codigo/api/src/main/java/med/voll/api/controller/MedicosController.java
+++ Codigo/api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -3,14 +3,14 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.medicos.DadosCadastroMedicos;
 
+import med.voll.api.medicos.DadosListagemMedico;
 import med.voll.api.medicos.Medico;
 import med.voll.api.medicos.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -23,5 +23,10 @@ public class MedicosController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
